a64dcf098dcb7a060ebe27153ba8459caf429336
Ravi Singh <rsingh60468@gmail.com>
2026-06-15 22:24:21 +0530

    added day 3 file

---
+++ day3.py
@@ -0,0 +1,48 @@
+# marks = [94.5, 56.5, 68.9, 36.8, 56.7]
+# print(marks)
+# print(type(marks))
+# print(marks[0], marks[1])
+# print(len(marks))
+# marks[0]=100
+# print(marks)
+# print(marks[1:4])
+
+# list = [2, 1, 3]
+# list.append(4)
+# list.sort()
+# list.sort(reverse = True)
+# list.reverse()
+# list.insert(4, 2)
+# list.remove(2)
+# list.pop(2)
+# print(list)
+
+# tup = (2, 1, 3, 1)
+# print(tup[2])
+
+# tup = (1,)
+# print(type(tup))
+# tup2 = (1)
+# print(type(tup2))
+
+# tup = (2, 1, 3, 1)
+# print(tup.index(1))
+# print(tup.count(1))
+
+# mov1 = input("enter first fav movie : ")
+# mov2 = input("enter second fav movie : ")
+# mov3 = input("enter third fav movie : ")
+# list = [mov1, mov2, mov3] # should have use append !
+# print(list) 
+
+# list = [1, 2, 3, 2, 1]
+# dupe = list.copy()
+# dupe.reverse()
+# print(dupe == list)
+
+# tup = ("c", "d", "a", "a", "b", "b", "a")
+# print(tup.count("a"))
+
+list = ["c", "d", "a", "a", "b", "b", "a"]
+list.sort()
+print(list)
