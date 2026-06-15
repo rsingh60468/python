221f8794567229dd438fb431bb6aa2b0b45e4943
Ravi Singh <rsingh60468@gmail.com>
2026-06-15 18:50:20 +0530

    added day1 and day2 files

---
+++ day1.py
@@ -0,0 +1,74 @@
+# age = 20.0
+# name = "xyz"
+# val = True
+# print("my name is : ",name)
+# print("my age is : ",age)
+
+# print(type(name))
+# print(type(age))
+# print(type(val))
+
+##arithmatic operators
+
+# a=5
+# b=3
+# sum = a+b
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+##relational operators
+
+# a=20
+# b=30
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+
+# #assignment operators
+
+# num = 10
+# # num = num + 10 # num+=10
+# num %= 4
+# print(num)
+
+# # logical opeerators
+# a=50
+# b=30
+# print(not True)
+# print(not(a>b))
+
+# val1 = True
+# val2 = False
+# print(val1 or val2)
+
+##type convertion
+
+# a=2
+# b=4.25
+
+# sum = a+b
+# print(sum)
+
+## type casting 
+
+# a=int("2")
+# b=4.25
+# print(type(a))
+# print(a+b)
+
+
+## input
+
+# name = input("enter your name : ")
+# print(name)
+# print(type(name))
+
+age = int(input("enter your age : "))
+print(type(age))
---
+++ day2.py
@@ -0,0 +1,36 @@
+# strings and conditionals 
+
+# str1 = "this is a string.\nwe are \tcreating it in python"
+# print(str1)
+
+# a="hello"
+# b=" world"
+# print(a+b)
+# print(len(a+b))
+
+#indexing
+
+# str = "ravi singh"
+# ch = str[4]
+# print(ch)
+
+# # slicing 
+# str = "ravisingh"
+# print(str[1:len(str)])
+# print(str[-5 : -1])
+
+# str = input("enter name : ")
+# print(len(str))
+
+# str = "hi i am a $ symbol $99 and $44 is amount"
+# print(str.count("$"))
+
+#conditional stat
+
+age = 15
+if(age>18):
+    print("can vote")
+elif(age>15):
+    print("can't vote")
+else:
+    print("go and play")
