c0b50a214b7e38cc51f709c49681595e0c6ccedb
Ravi Singh <rsingh60468@gmail.com>
2026-06-17 14:08:42 +0530

    day 4 completed

--- day4.py
+++ day4.py
@@ -22,14 +22,60 @@
 # print(student["subject"])
 # print(student["subject"]["phy"])
 
-dict = {
-    "name" : "ravi",
-    "age" : 20,
-    "marks" : [98, 97, 95]
-}
+# dict = {
+#     "name" : "ravi",
+#     "age" : 20,
+#     "marks" : [98, 97, 95]
+# }
+# dict2 = {
+#     "city" : "jaipur",
+#     "state" : "rajasthan"
+# }
+# print(dict)
+# print(dict.keys())
+# print(list(dict.keys()))
+# print(dict.values())
+# print(dict.items())
 
-print(dict)
-print(dict.keys())
-print(list(dict.keys()))
-print(dict.values())
-print(dict.items())
+# print(dict["name2"]) #ERROR IN THIS CASE CODDE EXECUTION STOPS 
+# print(dict.get("name2")) #CORRECT WAY TO USE VALUES 
+
+# dict.update({"sub" : "phy"})
+# dict.update(dict2)
+# print(dict)
+
+#sets
+# collection = {1, 2, 3, 4, "hello", "world", 2}
+# print(type(collection))
+# print(len(collection))
+# collection.add(99)
+# collection.remove(4)
+# collection.remove(80)
+# collection.clear()
+# val = collection.pop()
+# print(collection)
+# print(val)
+
+# set1 = {1, 2, 3, 4}
+# set2 = {3, 4, 5, 6}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+# dict = {}
+# dict.update({"table" : ["a piece of furniture", "list of facts and figs"], "cat" : "a small animal"})
+# print(dict)
+
+# dict = {}
+# mark1 = int(input("enter marks of phy : "))
+# mark2 = int(input("enter marks of chem : "))
+# mark3 = int(input("enter marks of bio : "))
+# dict.update({"phy" : mark1, "chem" : mark2, "bio" : mark3})
+# print(dict)
+
+# set = {int(9), float(9.0)}
+set = {9, "9.0"} #CORRECT
+# a= int(9)
+# b= float(9.0)
+# print(a, b)
+# set = {a, b}
+print(set)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-2
+
+dict = {}
+mark1 = int(input("enter marks of phy : "))
+mark2 = int(input("enter marks of chem : "))
+mark3 = int(input("enter marks of bio : "))
+dict.update({"phy" : mark1, "chem" : mark2, "bio" : mark3})
+print(dict)
