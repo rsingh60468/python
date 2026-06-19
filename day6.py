cbd978a3cc0b0320799a35cfdac5ee1325519230
Ravi Singh <rsingh60468@gmail.com>
2026-06-19 23:31:01 +0530

    day 6 completed

--- day6.py
+++ day6.py
@@ -70,10 +70,37 @@
 
 # show(4)
 
-def fact(n):
-    if(n==0 or n==1):
-        return 1
-    else:
-        return n*fact(n-1)
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
+#     else:
+#         return n*fact(n-1)
     
-print(fact(6))
+# print(fact(6))
+
+# def calc_sum(n):
+#     if(n==0):
+#         return 0
+#     else:
+#         return calc_sum(n-1)+n
+    
+# print(calc_sum(10))
+
+# list = [0, 1, 2, 3, 4]
+# def list_all(i=0):
+#     if(i==len(list)):
+#         return 
+#     print(list[i])
+#     list_all(i+1)
+
+# print(list_all())
+
+def print_list(list, idx=0):
+    if(idx==len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+fruits=["mango", "banana", "apple", "litchi"]
+
+print_list(fruits)
