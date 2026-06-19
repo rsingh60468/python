8d60f382420916483642e6fd135e04754632bb68
Ravi Singh <rsingh60468@gmail.com>
2026-06-19 22:44:56 +0530

    clean file

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-dict = {}
-mark1 = int(input("enter marks of phy : "))
-mark2 = int(input("enter marks of chem : "))
-mark3 = int(input("enter marks of bio : "))
-dict.update({"phy" : mark1, "chem" : mark2, "bio" : mark3})
-print(dict)
