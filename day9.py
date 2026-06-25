d9dfb4df8dcb73c4ea2c5f36c7522d09aef9378b
Ravi Singh <rsingh60468@gmail.com>
2026-06-25 21:01:47 +0530

    plus one prob

--- day9.py
+++ day9.py
@@ -26,18 +26,51 @@
 ##this is ok but giving TLE
 
 
-nums = [1, 2, 2, 3]
-target = 2
-count = 0
-sub = []
-for el in nums:
-    if (el == target):
-        sub.append(1)
-    else:
-        sub.append(-1)
-print(sub)
-for i in range(len(sub)):
-    for j in range(i+1, len(sub)+1):
-        if sum(sub[i:j]) > 0:
-            count +=1
-print(count)
+# nums = [1, 2, 2, 3]
+# target = 2
+# count = 0
+# sub = []
+# for el in nums:
+#     if (el == target):
+#         sub.append(1)
+#     else:
+#         sub.append(-1)
+# print(sub)
+# for i in range(len(sub)):
+#     for j in range(i+1, len(sub)+1):
+#         if sum(sub[i:j]) > 0:
+#             count +=1
+# print(count)
+
+#large integer + 1
+
+# digit = [1, 2, 4]
+
+# digit.reverse()
+# fd = digit[0]
+# sd = digit[1]
+
+# if (digit[0] != 9):
+#     digit[0] += 1
+# else:
+#     digit[0] = 0
+#     digit[1] += 1
+
+# digit.reverse()
+# print(digit)
+
+arr = [9, 9, 9, 9]
+result = 0
+for num in arr:
+    result = result * 10 + num
+print(result)
+
+result += 1
+plus = []
+
+while result > 0:
+    plus.append(result % 10)
+    result = result // 10
+
+plus.reverse()
+print(plus)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+num = 12345
+result = [int(digit) for digit in str(num)]
+print(result)  # Output: [1, 2, 3, 4, 5]
