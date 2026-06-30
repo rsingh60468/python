b4dd9457dc8de0ae3a6149e911e5af8719266ca5
Ravi Singh <rsingh60468@gmail.com>
2026-06-30 21:31:48 +0530

    cleaned files

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-s = "abc"
-count = 0
-for i in range(len(s)):
-    for j in range(i+1, len(s)+1):
-        if "a" in s[i:j] and "b" in s[i:j] and "c" in s[i:j]:
-            count += 1
-print(count)
